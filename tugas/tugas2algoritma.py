cd4263c9148df24b3a45afa86379732f606f1959
Steven Setiawan <sweeterchemist71@gmail.com>
2023-03-17 21:38:13 +0700

    tugas2

---
+++ tugas/tugas2algoritma.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+
+
+# In[ ]:
+
+
+
+
