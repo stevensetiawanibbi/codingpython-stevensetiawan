9b92360020ce3ef772512522051af7f326f3ebe1
Steven Setiawan <sweeterchemist71@gmail.com>
2023-03-28 21:40:39 +0700

    tugas3

---
+++ tugas/tugas3 interview question big o notation (1).py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # interview question 1
+
+# In[1]:
+
+
+def foo(array):
+    sum = 0
+    product = 1
+    
+    for i in array:
+        sum += i
+        
+    for i in array:
+        product *= i
+    print("Sum = "+str(sum)+", Product = "+str(product))
+    
+ar1 = [1,2,3,4]
+foo(ar1)
+
+
+# # interview question 2
+
+# In[4]:
+
+
+def PrintPairs(array):
+    for i in array:
+        for j in array:
+            print(str(i)+","+str(j))
+
+
+# # interview question 3
+
+# In[5]:
+
+
+def printUnorderedPairs(array):
+    for i in range(0,len(array)):
+        for j in range(i+1,len(array)):
+            print(array[i] + "," + array[j])
+
+
+# # interview question 4
+
+# In[2]:
+
+
+def printUnorderedPairs(arrayA, arrayB):
+    for i in range (len(arrayA)):
+        for j in range(len(arrayB)):
+            if arrayA[i] < arrayB[j]:
+                print(str(arrayA[i]) + "," + str(arrayB[j]))
+                
+arrayA = [1,2,3,4,5]
+arrayB = [2,6,7,8]
+
+
+# # interview question 5
+
+# In[3]:
+
+
+def printUnorderedPairs(arrayA, arrayB):
+    for i in range (len(arrayA)):
+        for j in range(len(arrayB)):
+            for k in range(0,100000):
+                print(str(arrayA[i]) + "," + str(arrayB[j]))
+                
+# printUnorderedPairss(arrayA,arrayB)
+
+
+# # interview question 6
+
+# In[4]:
+
+
+def reverse(array):
+    for i in range(0, int(len(array)/2)):
+        other = len(array)-i-1
+        temp = array[i]
+        array[i] = array[other]
+        array[other] = temp
+    print(array)
+    
+reverse(arrayA)
+
+
+# # interview question 8
+
+# In[5]:
+
+
+def factorial(n):
+    if n < 0:
+        return -1
+    elif n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+print(factorial(3))
+
+
+# # interview question 9
+
+# In[12]:
+
+
+def allFib(n):
+    for i in range(n):
+        print(str(i)+":, " + str(fib(i)))
+    
+def fib(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fib(n-1) + fib (n-2)
+
+
+# # interview question 10
+
+# In[6]:
+
+
+def powerOf2(n):
+    if n < 1:
+        return 0
+    elif n == 1:
+        print(1)
+        return 1
+    else:
+        prev = powerOf2(int(n/2))
+        curr = prev*2
+        print(curr)
+        return curr
+    
+powersOf2(50)
+
+
+# In[ ]:
+
+
+
+
--- tugas/tugas3 interview question recursive.py
+++ tugas/tugas3 interview question recursive.py
@@ -3,24 +3,27 @@
 
 # # Interview question 1
 
-# In[2]:
+# In[10]:
 
 
 def sum_of_digits(n):
-    if n < 10:
+    assert n >= 0 and int(n) == n
+    if n in [0]:
         return n
     else:
         return n % 10 + sum_of_digits(n // 10)
-n = 123
+n = 56
 print(sum_of_digits(n))
 
 
 # # Interview question 2
 
-# In[3]:
+# In[13]:
 
 
 def power(x, n):
+    assert type(x) == int and x >= 0
+    assert type(n) == int and n >= 0
     if n == 0:
         return 1
     elif n % 2 == 0:
@@ -34,25 +37,28 @@ print(power(x, n))
 
 # # Interview question 3
 
-# In[4]:
+# In[15]:
 
 
 def gcd(a, b):
+    assert isinstance(a, int) and a >= 0
+    assert isinstance(b, int) and b >= 0
     if b == 0:
         return a
     else:
         return gcd(b, a % b)
-a = 12
+a = 24
 b = 8
 print(gcd(a, b))
 
 
 # # Interview question 4
 
-# In[6]:
+# In[18]:
 
 
 def decimal_to_binary(n):
+    assert isinstance (n, int) and n >= 0
     if n == 0:
         return 0
     else:
